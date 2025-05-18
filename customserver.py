f5b312f06233cfc95846d4f215409b40735682cd
Zella Urquhart <zella.urquhart@gmail.com>
2025-05-17 18:39:03 -0700

    Update custom server

--- customserver.py
+++ customserver.py
@@ -1,11 +1,12 @@
 from http import server
+import os
 
 class CustomHandler(server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if (not (self.path == "/")) and (not ("." in self.path)):
+        strippedPath = self.path[1:len(self.path)]
+        if not (os.path.isfile(strippedPath) or os.path.isdir(strippedPath+"/")):
             self.path += ".html"
-        print(self.path)
-        return super().do_GET()
+        super().do_GET()
 
 with server.HTTPServer(('', 8000), CustomHandler) as srvr:
      srvr.serve_forever()
